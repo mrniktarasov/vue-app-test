4f7cd54f9ca930679fda2c655bb5d9e4c02257a0
Nikita Tarasov <nikitatarasov987@gmail.com>
2020-02-28 23:28:13 +0300

    Готовое приложение

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class='navigation'>
+        <router-link to="/bookmark1" class='navigation-box'>Закладка 1</router-link>
+        <router-link to="/switch" class='navigation-box'>Switch</router-link>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Switch from './components/Switch.vue';
+import Bookmark1 from './components/Bookmark1';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+const routes = [
+    { path: '/bookmark1', component: Bookmark1 },
+    { path: '/switch', component: Switch }
+  ];
+
+  const router = new VueRouter({
+    routes
+  });
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  router,
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app {
+    display: flex;
+    flex-direction: row;
+}
+
+.navigation {
+    min-width: 30vw;
+    height: 100vh;
+    display: flex;
+    background: linear-gradient(to bottom, blueviolet, #DCA3E2);
+    flex-direction: column;
+    align-content: center;
+}
+
+.navigation-box {
+    padding: 20px;
+    border: #64004a solid 2px;
+    color: white;
+    text-decoration: none;
+}
+
+.navigation-box:hover {
+    background-color: #64004a;
+}
+
+.navigation-box:not(:first-child) {
+    border-top: none;
 }
 </style>
---
+++ src/components/Bookmark1.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="plug">
+        Тьма, пришедшая со Средиземного моря,
+        накрыла ненавидимый прокуратором город. 
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Bookmark1',
+}
+</script>
+
+<style scoped>
+.plug {
+    max-width: 60vw;
+    margin: 20px 10px 20px 50px;
+}
+</style>
